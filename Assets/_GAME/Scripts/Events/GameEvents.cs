3b5b0f90d7758baa5a5645599d1e98477ae93e92
abducabuk <abdurrahman_cabuk@hotmail.com>
2023-09-27 06:23:00 +0300

    Feat: Added GameBoot and GameEvents

---
+++ Assets/_GAME/Scripts/Events/GameEvents.cs
@@ -0,0 +1,12 @@
+﻿namespace _GAME.Scripts.Events
+{
+    public static class GameEvents
+    {
+        public const string ON_AREA_CLICK="ON_AREA_CLICK";
+        public const string ON_AREAS_SELECTED="ON_AREAS_SELECTED";
+        public const string ON_AREAS_DESELECTED="ON_AREAS_DESELECTED";
+        public const string ON_AREA_CROWD_SERVICE_FILLED="ON_AREA_CROWD_SERVICE_FILLED";
+        public const string ON_AREA_CROWD_SERVICE_MOVE="ON_AREA_CROWD_SERVICE_MOVE";
+
+    }
+}
---
+++ Assets/_GAME/Scripts/GameBoot.cs
@@ -0,0 +1,17 @@
+﻿using CbkSDK.Core.ServiceLocator.Attribute;
+using UnityEngine;
+
+namespace _GAME.Scripts
+{
+    [BootGame]
+    public class GameBoot
+    {
+        public GameBoot()
+        {
+            Application.targetFrameRate = 60;
+            // var commands = new CommandChain();
+            //  // var commands = new CommandGroup();
+            //  commands.Execute();
+        }
+    }
+}
