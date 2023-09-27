c6bd0c28b9bb6c1754a38d97df06c37fcee29737
abducabuk <abdurrahman_cabuk@hotmail.com>
2023-09-27 06:21:15 +0300

    Feat: Added ColorTypeObject and AreaUpDown

---
+++ Assets/_GAME/Scripts/Other/AreaUpDown.cs
@@ -0,0 +1,30 @@
+﻿using _GAME.Scripts.Mediators;
+using CbkSDK.Util.General;
+using UnityEngine;
+
+namespace _GAME.Scripts.Other
+{
+    public class AreaUpDown : BaseObjectUpdater<bool>
+    {
+        public const float UP_HEIGHT = 1.5f;
+        [SerializeField] private AreaMediator _areaMediator;
+        [SerializeField] private BaseMove _move;
+
+        protected override bool Value => _areaMediator.IsSelected;
+        protected override void OnValueUpdate()
+        {
+            UpdatePosition();
+        }
+        private void UpdatePosition()
+        {
+            if (value)
+            {
+                _move.SetDestinationY(UP_HEIGHT);
+            }
+            else
+            {
+                _move.ResetDestination();
+            }
+        }
+    }
+}
---
+++ Assets/_GAME/Scripts/Other/ColorTypeObject.cs
@@ -0,0 +1,10 @@
+using _GAME.Scripts.Services;
+using CbkSDK.Util.General;
+
+namespace _GAME.Scripts.Other
+{
+    public class ColorTypeObject : BaseEnumObject<ColorType>
+    {
+
+    }
+}
